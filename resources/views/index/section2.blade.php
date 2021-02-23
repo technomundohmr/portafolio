e60d57aa6907ea41535ec728467ec2aa6fac946f
Andres <Technomundohmr@gmail.com>
2021-02-23 18:30:04 -0500

    se ha realizado el banner de abajo

--- resources/views/index/section2.blade.php
+++ resources/views/index/section2.blade.php
@@ -1,4 +1,4 @@
-<section class="my-5">
+<section class="my-5 pb-5">
     <div class="container">
         <div class="row">
             <div class="col text-center my-5">
@@ -13,7 +13,7 @@
                 <p>Autodidacta 100%, con la filosofía de nunca parar de aprender. Con alta velocidad de aprendizaje y abierto al cambio y a las nuevas tecnologías.</p>
             </div>
             <div class="col-md-6 w-100 mx-auto" >
-                <img src="https://images.pexels.com/photos/1043473/pexels-photo-1043473.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" width="50%">
+                <img class="foto" src="https://images.pexels.com/photos/1043473/pexels-photo-1043473.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" width="50%">
             </div>
         </div>
     </div>
